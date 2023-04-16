45cd2a15e4df49ffb6ea26bb5cac450fddc88bc8
hasancelikoglu <hasancelikoglu92@gmail.com>
2023-04-17 00:14:11 +0300

    test fetch

--- client/app/page.js
+++ client/app/page.js
@@ -1,3 +1,14 @@
-export default function Home() {
-	return <div></div>;
+async function getData() {
+	const response = await fetch("http://localhost:3001");
+	const data = await response.json();
+	return data;
+}
+
+export default async function Home() {
+	const data = await getData();
+	return (
+		<div>
+			<h1>test message : {data.message}</h1>
+		</div>
+	);
 }
