e43324b67a8072d295ff7c3cf0699ac404a8dfc9
hasancelikoglu <hasancelikoglu92@gmail.com>
2023-04-19 21:13:18 +0300

    feat: login page

--- client/components/login-card/index.js
+++ client/components/login-card/index.js
@@ -1,37 +1,41 @@
 import Link from "next/link";
+import { IoArrowForward } from "react-icons/io5";
 
 export default function LoginCard() {
 	return (
-		<div className="w-1/3 px-20 h-3/5 flex flex-col justify-between p-10">
-			<h1 className="text-center text-white-full text-xl font-light">
+		<div className="w-1/3 h-3/5 flex flex-col items-center justify-between py-10 px-16">
+			<h1 className="text-center text-white-full text-2xl font-light">
 				Login to app
 			</h1>
-			<form className="flex flex-col gap-y-5">
+			<form className="flex flex-col w-full items-center gap-y-5">
 				<input
 					type="text"
 					placeholder="Enter your email"
-					className="p-4 shadow-inner rounded-xl outline-none"
+					className="p-4 shadow-inner rounded-xl outline-none w-full"
 				/>
 				<input
 					type="text"
 					placeholder="Enter your password"
-					className="p-4 shadow-inner rounded-xl outline-none"
+					className="p-4 shadow-inner rounded-xl outline-none w-full"
 				/>
-				<button type="submit" className="text-white-full">
-					Giriş Yap
+				<button
+					type="submit"
+					className="text-dark bg-white-full p-3 rounded-xl"
+				>
+					<IoArrowForward size={20} />
 				</button>
 			</form>
 
 			<div className="flex flex-col items-center justify-center gap-y-3">
 				<Link
 					href="/"
-					className="text-white-t font-light border-b border-b-white-t"
+					className="text-white-t font-light border-b border-b-white-t text-sm"
 				>
 					Don't have an account? let's create!
 				</Link>
 				<Link
 					href="/"
-					className="text-white-t font-light border-b border-b-white-t"
+					className="text-white-t font-light border-b border-b-white-t text-sm"
 				>
 					Forgot password?
 				</Link>
--- client/components/sprinkle/index.js
+++ client/components/sprinkle/index.js
@@ -7,8 +7,16 @@ export default function Sprinkle() {
 				src="/sprinkle.svg"
 				fill
 				unoptimized
-				className="absolute h-full w-full object-cover"
+				className="absolute h-full w-full object-cover -z-10"
 			/>
+			<div className="h-full w-full flex flex-col items-center justify-center gap-y-5">
+				<h1 className="text-5xl font-bold text-white-full drop-shadow-xl">
+					SOCITY
+				</h1>
+				<p className="text-white-t font-light text-lg">
+					Communicate with your friends and all the people in the world.
+				</p>
+			</div>
 		</div>
 	);
 }
