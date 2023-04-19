10d811d46ef956a5a9ce3de8bbe8ce61a3d4b434
hasancelikoglu <hasancelikoglu92@gmail.com>
2023-04-19 20:20:37 +0300

    feat: start of the login page

--- client/app/page.js
+++ client/app/page.js
@@ -1,14 +1,3 @@
-async function getData() {
-	const response = await fetch("http://localhost:3001");
-	const data = await response.json();
-	return data;
-}
-
 export default async function Home() {
-	const data = await getData();
-	return (
-		<div>
-			<h1>test message 123 : {data.message}</h1>
-		</div>
-	);
+	return <div></div>;
 }
